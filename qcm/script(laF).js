e46b4bb4299bd6f68b2fefe007c8602af9727b8a
lomac20 <152649910+LoreneMadelaineCaprice@users.noreply.github.com>
2024-02-12 19:31:11 -0500

    COMMIT

---
+++ qcm/script(laF).js
@@ -0,0 +1,90 @@
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+}
+let questions = [
+  new Question("A quel siècle appartient La Fontaine?", ["15e", "18e", "16e", "17e"], "17e"),
+  new Question("A quel mouvement appartiennent les oeuvres de la Fontaine?", ["lumières","romantisme", "classicisme", "gothique"], "classicisme"),
+  new Question("Qu'est-ce que la Fontaine n'est pas ?", ["Poète","comédien", "moraliste", "romancier"], "comédien"),
+  new Question("De quel institue a-t'il été membre ?", ["ac scientifiques","ac latinistes", "aucune", "Ac française"], "Ac française")
+];
+
+console.log(questions);
+
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+  }
+  getCurrentQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  guess(answer) {
+    if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
+      this.score++;
+    }
+    this.currentQuestionIndex++;
+  }
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
+}
+
+// Regroup all  functions relative to the App Display
+const display = {
+  elementShown: function(id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+  endQuiz: function() {
+    endQuizHTML = `
+      <h1>Quiz terminé !</h1>
+      <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
+    this.elementShown("quiz", endQuizHTML);
+  },
+  question: function() {
+    this.elementShown("question", quiz.getCurrentQuestion().text);
+  },
+  choices: function() {
+    let choices = quiz.getCurrentQuestion().choices;
+
+    guessHandler = (id, guess) => {
+      document.getElementById(id).onclick = function() {
+        quiz.guess(guess);
+        quizApp();
+      }
+    }
+    // display choices and handle guess
+    for(let i = 0; i < choices.length; i++) {
+      this.elementShown("choice" + i, choices[i]);
+      guessHandler("guess" + i, choices[i]);
+    }
+  },
+  progress: function() {
+    let currentQuestionNumber = quiz.currentQuestionIndex + 1;
+    this.elementShown("progress", "Question " + currentQuestionNumber + " sur " + quiz.questions.length);
+  },
+};
+
+// Game logic
+quizApp = () => {
+  if (quiz.hasEnded()) {
+    display.endQuiz();
+  } else {
+    display.question();
+    display.choices();
+    display.progress();
+  } 
+}
+// Create Quiz
+let quiz = new Quiz(questions);
+quizApp();
+
+console.log(quiz);
